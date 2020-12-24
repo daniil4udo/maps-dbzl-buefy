9fcef29cfec22d37bcb71ad6a4521ec02910c09d
Daniil <daniil.chumachenko@democrance.com>
2020-12-24 14:22:28 +0400

    feat: enhance object keys

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,15 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+declare module '*.scss';
+
+type ObjectKeys<T> =
+  T extends object ? (keyof T)[] :
+  T extends number ? [] :
+  T extends Array<any> | string ? string[] :
+  never;
+
+declare interface ObjectConstructor {
+  keys<T>(o: T): ObjectKeys<T>;
+}
